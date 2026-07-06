4aa9695749abd2f83716e39519476cd0206bb5ef
arunkudiyal <arunkudiyal@icloud.com>
2026-07-06 12:19:26 +0530

    BST - search & height

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -21,9 +21,17 @@ public class BinarySearchTree {
     }
 
     public TreeNode search(TreeNode root, int val) {
+        if(root != null && root.data != val) System.out.println(root.data);;
         if(root == null || val == root.data) return root;
         else if(val < root.data) return search(root.left, val);
         else return search(root.right, val);
+    }
+
+    public int height(TreeNode root) {
+        if(root == null) return 0;
+        int lHeight = height(root.left);
+        int rHeight = height(root.right);
+        return Math.max(lHeight, rHeight) + 1;
     }
 
     public void inOrder(TreeNode node) {
@@ -48,6 +56,10 @@ public class BinarySearchTree {
         bst.root = bst.insertData(bst.root, 3);
         bst.root = bst.insertData(bst.root, 50);
 
-        bst.inOrder(bst.root);          //
+        bst.inOrder(bst.root);
+        System.out.println();
+        bst.search(bst.root, 30);
+
+        System.out.println( bst.height(bst.root) );     // 5
     }
 }
